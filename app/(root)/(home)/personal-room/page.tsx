f477df0d9657eacf678b10d197a5498df4451efe
vilmao0 <koller.vilmos.sandor@outlook.com>
2024-05-18 23:24:39 +0200

    Navbar1

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const PersonalRoom = () => {
   return (
-    <div>PersonalRoom</div>
+    <section className='flex size-full flex-col gap-10 text-white'>
+      <h1 className='text-3xl font-bold'>
+        MyRoom™️
+      </h1>
+    
+    </section>
   )
 }
 
---
+++ components/MobileNav.tsx
@@ -0,0 +1,40 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from "next/image"
+  import React from 'react'
+
+const MobileNav = () => {
+  return (
+    <section className='w-full max-w-[264px]'>
+        <Sheet>
+            <SheetTrigger asChild>
+                <Image 
+                width={36}
+                height={36}
+                src="/icons/hamburger.svg"
+                alt="hamburger icon"
+                className="cursor-pointer sm:hidden"
+                />
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                    This action cannot be undone. This will permanently delete your account
+                    and remove your data from our servers.
+                </SheetDescription>
+                </SheetHeader>
+        </SheetContent>
+        </Sheet>
+
+    </section>
+  )
+}
+
+export default MobileNav
