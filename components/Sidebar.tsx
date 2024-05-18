7213e35f36a68796db3f0dc44057d892be838edb
vilmao0 <koller.vilmos.sandor@outlook.com>
2024-05-18 22:43:31 +0200

    Adding Routes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import React from 'react'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
 
 const Sidebar = () => {
     const pathname = usePathname();
@@ -22,7 +23,15 @@ const Sidebar = () => {
 
                     })}
                     >
-                        {link.label}
+                        <Image 
+                            src={link.imgUrl}
+                            alt={link.label}
+                            width={24}
+                            height={24}
+                        />
+                        <p className='text-lg font-semibold max-lg:hidden'>
+                            {link.label}
+                        </p>
                     </Link>
                 )
             })}
