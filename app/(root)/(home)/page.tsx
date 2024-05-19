1af10ae73f29036392bc4f4cd8c001aef72ff17d
vilmao0 <koller.vilmos.sandor@outlook.com>
2024-05-19 14:24:31 +0200

    Home page2

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,10 +3,16 @@ import React from 'react'
 const Home = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-3xl font-bold'>
-        Home
-      </h1>
-    
+      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'></div>
+      <div className='flex h-full flex-col justify-between max-md:px5 max-md:py-8 lg:p-11'>
+        <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>Next meeting at: 13:25</h2>
+        <div className='flex flex-col gap-2'>
+          <h1>
+            11:30
+          </h1>
+          <p></p>
+        </div>
+      </div>
     </section>
   )
 }
