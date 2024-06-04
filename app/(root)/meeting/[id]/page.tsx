47176f501edb230dfb8b53af5aad1097f7cdb217
vilmao0 <koller.vilmos.sandor@outlook.com>
2024-06-04 19:56:36 +0200

    editing meeting

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Meeting = ({ params }: { params: { id: string } }) => {
   return (
-    <div>Meeting Room: #{params.id}</div>
+    <div className='text-xl'>Meeting Room: #{params.id} <p className='text-teal-200 text-xl'>for more info see:</p> <a href='https://help1.vilmao.org/info/meeting/connecting-with-mtngid'>
+        <p className='font-bold text-emerald-400 text-3xl'>
+          help.vilmao.org
+        </p>
+      </a></div>
   )
 }
 
